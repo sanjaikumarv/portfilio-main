ef7d92b2852fadac28c11883462c33ec98ee4998
Sanjaikumar v <80317211+sanjaikumarv@users.noreply.github.com>
2025-07-10 11:30:08 +0530

    feat : reference error fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-import About from "@/components/About"
-import BackgroundAnimation from "@/components/BackgroundAnimation"
-import Certificates from "@/components/Certificates"
-import Contact from "@/components/Contact"
-import Education from "@/components/Education"
-import Experience from "@/components/Experience"
-import Footer from "@/components/Footer"
-import Header from "@/components/Header"
-import Hero from "@/components/Hero"
-import Projects from "@/components/Projects"
-import Skills from "@/components/Skills"
-
+import About from "@/components/About";
+import BackgroundAnimation from "@/components/BackgroundAnimation";
+import Certificates from "@/components/Certificates";
+import Contact from "@/components/Contact";
+import Education from "@/components/Education";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
